9f48a4134b06d4b208ac08e976b554ab117bd2ed
Alex <alexkim98@korea.ac.kr>
2022-11-05 23:14:27 +0900

    Project skeleton

---
+++ target/debug/deps/zero2prod-5d22ce87b8040258.d
@@ -0,0 +1,7 @@
+/Users/alexkim/Developer/Code/zero2prod/target/debug/deps/zero2prod-5d22ce87b8040258.rmeta: src/main.rs
+
+/Users/alexkim/Developer/Code/zero2prod/target/debug/deps/zero2prod-5d22ce87b8040258.d: src/main.rs
+
+src/main.rs:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__
---
+++ target/debug/deps/zero2prod-7b41419f4e8bd71c.d
@@ -0,0 +1,5 @@
+/Users/alexkim/Developer/Code/zero2prod/target/debug/deps/zero2prod-7b41419f4e8bd71c: src/main.rs
+
+/Users/alexkim/Developer/Code/zero2prod/target/debug/deps/zero2prod-7b41419f4e8bd71c.d: src/main.rs
+
+src/main.rs:
